dcd1770b60b3eea1250bbe99132b0eb551f9ce2c
Aluno <joao.pv09@aluno.ifsc.edu.br>
2024-04-17 20:11:17 -0300

    LISTA06

---
+++ lista6/src/lista06/Produto.java
@@ -0,0 +1,22 @@
+package lista06;
+
+public class Produto  {
+
+	 private String fabricante;
+	 private Long codeBarras;
+	
+	
+	
+	public void setFabricante(String fabricante) {
+		this.fabricante = fabricante;
+	}
+	public Long getCodeBarras() {
+		return codeBarras;
+	}
+	public void setCodeBarras(Long codebarras) {
+		codeBarras = codebarras;
+	}
+	public String getFabricante() {
+		return fabricante;
+	}
+}
